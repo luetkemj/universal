3a03a3d616947252dcda19000ad7782b07e535a6
luetkemj <luetkemj@gmail.com>
2018-07-22 21:48:46 -0500

    remove page-data.controller
    this is a demo artifact for loading data on the server. Will update with apollo if necessary later.

--- app/controllers/page-data.controller.js
+++
@@ -1,9 +0,0 @@
-export function loadPage1Data(req, res) {
-  setTimeout(() => {
-    res.send({
-      data: {
-        pageNumber: 1,
-      },
-    });
-  }, 1000);
-}
--- app/routes/express-routes.js
+++ app/routes/express-routes.js
@@ -1,9 +1,6 @@
-import { loadPage1Data } from '../controllers/page-data.controller';
 import render from '../controllers/render.controller';
 
 module.exports = (router) => {
-  router.route('/api/page/1').get(loadPage1Data);
-
   // if at this point we don't have a route match for /api, return 404
   router.route('/api/*').all((req, res) => res.status(404).send({
     error: `route not found: ${req.url}`,
