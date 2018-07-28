1e479a7ecbe50cc5b0c78c27500bd04ba41a8d82
luetkemj <luetkemj@gmail.com>
2018-07-28 08:28:48 -0500

    Add Apollo Client to client

--- app/config/client.js
+++ app/config/client.js
@@ -2,14 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 
 import App from '../containers/app/app.container';
 
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+});
+
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>,
     document.getElementById('app'),
   );
 }
